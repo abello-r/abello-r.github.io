6745a4a18098690f5e8abad374d288b81cbb56ad
abello-r <alexanderbellor18@gmail.com>
2021-07-14 13:26:51 +0200

    test 6.22

--- back/server.js
+++ back/server.js
@@ -26,40 +26,6 @@ app.get('/private', (req, res) => {
 	})
 });
 
-let api_token = '03541c39e9d7791c7d18fed22d2039302f0a4ab0853f0d64e3c63a97f3b83d08';
-
-app.get('/code', async (req, res) => {
-	const Token_Endpoint = `https://api.intra.42.fr/oauth/token`;
-	const Grant_Type = 'authorization_code';
-	// const Grant_Type = 'client_credentials';
-	const Code = api_token;
-	const Redirect_Uri = 'https://42ring.es';
-	const Client_Id = process.env.UID;
-	const Client_Secret = process.env.SECRET;
-	const Scope = "public";
-
-	// Check State to avoid security errors
-	if (req.query.state != 'ThisIsMyStateValue')
-	{
-		res.redirect('/');
-	}
-
-	// log.info(`Body: ${body}`);
-	let json = await axios.post(Token_Endpoint,
-		{
-			grant_type: Grant_Type,
-			// grant_type: 'client_credentials',
-			code: Code,
-			redirect_uri: Redirect_Uri,
-			client_id: Client_Id,
-			client_secret: Client_Secret,
-			scope: Scope
-		},
-		{ headers: { 'Content-Type': 'application/json' } });
-	//console.log(Code)
-	console.log(json)
-})
-
 app.listen(config.PORT, () => {
 	console.log(`\
 
