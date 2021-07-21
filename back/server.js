7c7ab9f3d6864ba4bc8edbb373685273d6e68845
abello-r <alexanderbellor18@gmail.com>
2021-07-21 10:27:14 +0000

    move bad request down

--- back/server.js
+++ back/server.js
@@ -29,55 +29,42 @@ app.get("/ring", getBearer, (req, res) => {
 	res.sendFile(path.resolve("../public/ring.html"))	// Ring Zone
 })
 
-app.get("/private", getBearer, (req, res) =>
-{
-	res.json({
-		token: process.env.BEARER_TOKEN,				// Hidden Token
-		info: "This route will not available soon",
-	})
-})
-
-app.get("*", (req, res) =>
-{
-	res.send("404 Page not found, come back buddy :(")									// All Bad Requests
-})
-
 /*--------------------------------------------------------------------------------*/
 
 
 /*
- ** GET /{login1}/${login2}
- ** Returns the power of each login with a status of 200
- ** 	and all the data (login and power) is sent in a json array.
- ** In case of same login, returns an error and a status 418
- ** If any login does not exist, then a status of 404 is returned
- ** 	and an error mesage inside "error" in json
- */
+** GET /{login1}/${login2}
+** Returns the power of each login with a status of 200
+** 	and all the data (login and power) is sent in a json array.
+** In case of same login, returns an error and a status 418
+** If any login does not exist, then a status of 404 is returned
+** 	and an error mesage inside "error" in json
+*/
 
 app.get("/:login1/:login2", getBearer, (req, res, next) =>
 {
 	if (req.originalUrl.startsWith("/404"))
-		return next()
+	return next()
 	if (req.params.login1 === req.params.login2)
-		return res
-			.status(418)
-			.json(`Fighting ${req.params.login1} vs ${req.params.login2}...`)
+	return res
+	.status(418)
+	.json(`Fighting ${req.params.login1} vs ${req.params.login2}...`)
 	const options =
 	{
 		method: "GET",
 		url: `https://api.intra.42.fr/v2/users/${req.params.login1}`,
 		headers: {	Authorization: `Bearer ${process.env.BEARER_TOKEN}` },
 	}
-
+	
 	request(options, async (error, response, body) =>
 	{
 		if (error) throw new Error(error)
-
+		
 		console.log(body)
 		if (response.statusCode != 200)
-			return res
-				.status(404)
-				.json({ error: `${req.params.login1} is not a valid user` })
+		return res
+		.status(404)
+		.json({ error: `${req.params.login1} is not a valid user` })
 		const parsed1 = await JSON.parse(body)
 		const options =
 		{
@@ -88,32 +75,32 @@ app.get("/:login1/:login2", getBearer, (req, res, next) =>
 		request(options, async (error, response, body) =>
 		{
 			if (error) throw new Error(error)
-
+			
 			console.log(body)
 			if (response.statusCode != 200)
-				return res
-					.status(404)
-					.json({ error: `${req.params.login2} is not a valid user` })
-					const parsed = await JSON.parse(body)
-					
-
+			return res
+			.status(404)
+			.json({ error: `${req.params.login2} is not a valid user` })
+			const parsed = await JSON.parse(body)
+			
+			
 			/*------------------------------------------------------------------------------*/		
 			// Calculate POWER PLAYER ONE
 			const power1 =
-				parsed1.correction_point + 
-				parsed1.wallet +
-				parsed1.achievements.length +
-				parsed1.projects_users.length * 0.5
+			parsed1.correction_point + 
+			parsed1.wallet +
+			parsed1.achievements.length +
+			parsed1.projects_users.length * 0.5
 			
 			// Calculate POWER PLAYER TWO
 			const power2 =
-				parsed.correction_point +
-				parsed.wallet +
-				parsed.achievements.length +
-				parsed.projects_users.length * 0.5
+			parsed.correction_point +
+			parsed.wallet +
+			parsed.achievements.length +
+			parsed.projects_users.length * 0.5
 			/*------------------------------------------------------------------------------*/
-
-				res.status(200).json([
+			
+			res.status(200).json([
 				{
 					login: parsed1.login,
 					power: power1,
@@ -131,17 +118,29 @@ app.get("/:login1/:login2", getBearer, (req, res, next) =>
 	})
 })
 
+app.get("/private", getBearer, (req, res) =>
+{
+	res.json({
+		token: process.env.BEARER_TOKEN,				// Hidden Token
+		info: "This route will not available soon",
+	})
+})
 
-/*-----------------------------------App Listen--------------------------------------------*/
+app.get("*", (req, res) =>
+{
+	res.send("404 Page not found, come back buddy :(")									// All Bad Requests
+})
+
+/*-----------------------------------App Listen on Port--------------------------------------------*/
 
 app.listen(config.PORT, () =>
 {
 	console.log(
 		`Started [PORT ${config.PORT}] http://${config.HOST}:${config.PORT}`
-	)
-
-	console.log(`If you have this problem [localhost refused to connect],\
-	Check that in the backend directory, the .env file has the correct data.`)
-})
-
+		)
+		
+		console.log(`If you have this problem [localhost refused to connect],\
+		Check that in the backend directory, the .env file has the correct data.`)
+	})
+	
 /*---------------------------------------------------------------------------------------------*/
